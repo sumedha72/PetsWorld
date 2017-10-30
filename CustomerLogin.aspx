bbd027a4581e9f0de50e4ba5facbfd510447c8ba
Shreya495 <33148856+Shreya495@users.noreply.github.com>
2017-10-30 16:30:58 +0530

    Add files via upload

---
+++ CustomerLogin.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomerLogin.aspx.cs" Inherits="WebApplication6.CustomerLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-3.2.1.js"></script>
+    <a href="Scripts/jquery-3.2.1.min.map"></a>
+    <script src="Scripts/jquery-3.2.1.min.js"></script>
+    <script src="Scripts/jquery-ui-1.12.1.js"></script>
+    <script src="Scripts/jquery.validate.js"></script>
+     <script type="text/javascript" language="javascript">
+         function Validate() {
+             var uname = document.getElementById('txtuname');
+             var Password = document.getElementById('txtpswd');
+             if ((uname.value == '') || (Password.value == '')) {
+                 alert('UserName or Password should not be blank');
+                 return false;
+             }
+             else {
+                 return true;
+             }
+         }
+    </script>
+    </head>
+   <h1>CUSTOMER LOGIN</h1>
+    <label> Sign in to continue  </label>
+       
+<form id="form1" runat="server">
+        <table>
+            <tr>
+                <td>
+          <label>
+            USERNAME:
+        </label>
+         <asp:TextBox runat="server" ID="txtuname" placeholder="username" /><asp:RequiredFieldValidator ID="txtuanme" ForeColor="Red"  ControlToValidate="txtuname" runat="server" ErrorMessage="Username required"></asp:RequiredFieldValidator>  <br /><br />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+         <label>
+            PASSWORD:
+        </label> 
+         <asp:TextBox runat="server" ID="txtpswd" TextMode="Password" placeholder="password"></asp:TextBox>
+        <%--<asp:RequiredFieldValidator ID="txtpswrd" ControlToValidate="txtpswd" ForeColor="Red" runat="server" ErrorMessage="Password required"></asp:RequiredFieldValidator>--%>
+          <br /><br /> 
+                    </td>
+        </tr>
+            <tr>
+                <td>
+                   
+                    <asp:CheckBox runat="server" ID="chkremember" />
+                     <label>
+                        Remember me
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                   <asp:Button ID="btn_login" runat="server" Text="Login" Font-Bold="True"
+            BackColor="#CCFF99"   OnClientClick="Validate()"/><br />
+                </td>
+            </tr>
+
+        </table>
+     
+      
+
+    <div>
+    
+    </div>
+                  
+          
+    </form>
+</body>
---
+++ CustomerLogin.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Book_Store.App_Code;
+
+namespace Book_Store
+{
+    public partial class CustomerLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                HttpCookie UserInfo = Request.Cookies["UserInfo"];
+                if (UserInfo != null)
+                {
+                    txtuname.Text = UserInfo["username"].ToString();
+                    txtpswd.Attributes.Add("value", UserInfo["password"].ToString());
+                }
+            }
+
+        }
+
+
+
+        protected void btn_login_Click(object sender, EventArgs e)
+        {
+            String hashpassword = PasswordClass.Hashpassword(txtpswd.Text);
+            SqlConnection con = new SqlConnection("Data Source=WIN-T5M51LAKF72\\MSSQLSERVER2; Initial Catalog=BookStore; Integrated Security=true;");
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("select * from customer where username=@username and password=@pasword", con);
+            cmd.Parameters.AddWithValue("@username", txtuname.Text);
+            cmd.Parameters.AddWithValue("@pasword", hashpassword);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            if (chkremember.Checked)
+            {
+                HttpCookie cookie = new HttpCookie("userinfo");
+
+                cookie["username"] = txtuname.Text;
+                cookie["password"] = hashpassword;
+
+                cookie.Expires = DateTime.Now.AddDays(7);
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                HttpCookie cookie = new HttpCookie("userinfo");
+                if (cookie != null)
+                {
+                    cookie.Expires = DateTime.Now.AddDays(-1);
+                    Response.Cookies.Add(cookie);
+                }
+            }
+            if (dt.Rows.Count > 0)
+            {
+                Session["username"] = txtuname.Text;
+                Session["password"] = hashpassword;
+                Session["Logstatus"] = true;
+                Response.Redirect("customerview.aspx");
+            }
+
+            else
+            {
+                Response.Write("Invalid Credentials");
+            }
+        }
+
+
+    }
+}

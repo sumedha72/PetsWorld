1c7311b2879497fc7e7f9093487f2f2869e03cce
AllanAj5 <allan1ralph@gmail.com>
2017-10-30 12:57:07 +0530

    Add files via upload
    Allan MASTER

---
+++ Mas.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Home
+{
+    public partial class Mas : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Llogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void searchBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

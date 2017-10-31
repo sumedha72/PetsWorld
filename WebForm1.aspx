e87aa55f1375c294aca4791d03c71c7fa3ad3eeb
Shreya495 <33148856+Shreya495@users.noreply.github.com>
2017-10-31 14:14:46 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication7.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server" AsyncPostBackTimeout="600">
+        </asp:ScriptManager>
+        <asp:Timer ID="Timer1" Interval="3000" OnTick="Timer1_Tick" Enabled="false" runat="server">
+        </asp:Timer>
+        <br />
+        <asp:UpdatePanel ID="updPanel" runat="server" UpdateMode="Conditional">
+            <ContentTemplate>
+                     <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Auto Send Mail Test" />     
+            </ContentTemplate>
+            <Triggers>
+                     <asp:AsyncPostBackTrigger ControlID="Timer1" EventName="Tick" />
+            </Triggers>
+        </asp:UpdatePanel>
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;//for background worker class
+using Microsoft.VisualBasic;//for interacation message box
+using System.Net;
+using System.Net.Mail;
+
+namespace WebApplication7
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+         DateTime hours;
+    BackgroundWorker bw = new BackgroundWorker();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            hours = Convert.ToDateTime(DateTime.Now.TimeOfDay.ToString());
+            hours = hours.AddMinutes(1);
+            Session["maxIssue"] = hours;
+            Response.Redirect(Session["maxIssue"].ToString());
+            Response.Redirect("Sesssion Saved Time");
+        }
+    }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Button1.Enabled = false;
+            Timer1.Enabled = true;
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+             bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+        bw.WorkerReportsProgress = false;
+        bw.WorkerSupportsCancellation = true;
+        bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        bw.RunWorkerAsync();
+    }
+    public void bw_DoWork(object sender, DoWorkEventArgs e)
+    {
+        Session.Timeout = 20;
+        DateTime d = Convert.ToDateTime(Session["maxIssue"].ToString());
+        //Interaction.MsgBox("D value is :" + d, MsgBoxStyle.Information, "Date Value");
+        if (d <= DateTime.Now)
+        {
+            Timer1.Enabled = false;//for stopping the timer once the mail is sent.
+            Response.Redirect("Mail Sent"+ DateTime.Now.ToString());
+            //Interaction.MsgBox("Mail Sent" + DateTime.Now.ToString(), MsgBoxStyle.Information, "Mail Sent");
+            SendMail();
+        }
+        }
+
+          public void SendMail()
+    {
+        MailMessage msg = new MailMessage();
+        msg.From = new MailAddress("shreya.das.495@gmail.com");
+        msg.To.Add("sumedha72.95@gmail.com");
+        msg.Body = "Testing the automatic mail";
+        msg.IsBodyHtml = true;
+        msg.Subject = "Movie Data";
+        SmtpClient smt = new SmtpClient("smtp.gmail.com");
+        smt.Port = 587;
+        smt.Credentials = new NetworkCredential("your login account", "your word");
+        smt.EnableSsl = true;
+        smt.Send(msg);
+        string script = "<script>alert('Mail Sent Successfully');self.close();</script>";
+        this.ClientScript.RegisterClientScriptBlock(this.GetType(), "sendMail", script);
+    }
+          public static void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+    }
+    }
+}
